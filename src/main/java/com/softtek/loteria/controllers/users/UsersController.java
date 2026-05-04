58ca629e2bf59eeeb23c1bcbcd25d1d8b2c78f9e
Aaron Granado Vargas <aaron.granado@softtek.com>
2026-05-04 15:19:05 +0200

    Cambio y mejoras

--- src/main/java/com/softtek/loteria/controllers/users/UsersController.java
+++ src/main/java/com/softtek/loteria/controllers/users/UsersController.java
@@ -1,9 +1,13 @@
 package com.softtek.loteria.controllers.users;
 
+import com.softtek.loteria.model.Bet;
+import com.softtek.loteria.model.User;
+import org.springframework.http.ResponseEntity;
+
 import java.util.List;
 
 public interface UsersController {
-    ResponseEntity<Void> registerUser(String dni, User user);
+    ResponseEntity<Void> registerUser(String dni, String name);
 
     User getUserByDni(String dni);
 
--- src/main/java/com/softtek/loteria/services/user/UserServiceImpl.java
+++ src/main/java/com/softtek/loteria/services/user/UserServiceImpl.java
@@ -1,35 +1,32 @@
 package com.softtek.loteria.services.user;
 
-import com.softtek.loteria.models.User;
-import com.softtek.loteria.repositories.UserRepository;
+import com.softtek.loteria.model.User;
+import com.softtek.loteria.repository.user.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class UserServiceImpl
-{
+public class UserServiceImpl implements UserService {
 
-    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     private final UserRepository userRepository;
 
-    public UserServiceImpl(UserRepository userRepository)
-    {
+    public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    public User createUser(String id, String name)
-    {
-        if (userRepository.existsById(id))
-        {
+    public User createUser(String dni, String name) {
+        if (userRepository.existsByDni(dni)) {
             logger.error("Intento de crear usuario duplicado");
             throw new RuntimeException("User already exists");
         }
 
-        User user = new User(id, name);
+        User user = new User(dni, name, new ArrayList<>());
         userRepository.save(user);
 
         logger.info("Usuario creado");
@@ -38,18 +35,16 @@ public class UserServiceImpl
         return user;
     }
 
-    public List<User> getAllUsers()
-    {
+    public List<User> getAllUsers() {
         logger.info("Obteniendo lista de usuarios");
         return userRepository.findAll();
     }
 
-    public User getUserById(String id)
-    {
-        return userRepository.findById(id).orElseThrow(() ->
-                {
-                    logger.error("Usuario no encontrado");
-                    return new RuntimeException("User not found");
-                });
+    public User getUserById(String id) {
+        return userRepository.findByDni(id).orElseThrow(() ->
+        {
+            logger.error("Usuario no encontrado");
+            return new RuntimeException("User not found");
+        });
     }
 }
